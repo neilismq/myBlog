fb19b9847b73d2be6d83de09b10bbbf38c896eca
zhaozhiqiang <zhaozhiqiang@kingxunlian.com>
2018-11-15 16:44:21 +0800

    â秀çä修改pomã.xml

--- src/main/java/com/bj/zzq/TestApplication.java
+++ src/main/java/com/bj/zzq/TestApplication.java
@@ -1,7 +1,11 @@
 package com.bj.zzq;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TestApplication {
     public static void main(String[] args){
-        System.out.println("fsdfsd");
+        SpringApplication.run(TestApplication.class,args);
     }
 }
