b16d3d01c21d96bc670dca6a44da9de735764a51
zhaozhiqiang <zhaozhiqiang@kingxunlian.com>
2018-12-12 16:04:32 +0800

    测试数据库成功

--- src/main/java/com/bj/zzq/controller/ArticalController.java
+++ src/main/java/com/bj/zzq/controller/ArticalController.java
@@ -1,21 +1,37 @@
 package com.bj.zzq.controller;
 
+import com.bj.zzq.model.ArticleEntity;
+import com.bj.zzq.model.ArticleEntityExample;
+import com.bj.zzq.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 
 @Controller
 @RequestMapping
 public class ArticalController {
-
+    @Autowired
+    private BlogService blogService;
 
     @RequestMapping("/")
-    public String test() {
+    public String test(Map map) {
         /**
          * 此处相当于又发了一次，http://localhost:8888/index.html的请求，然后映射到静态页面/static/index.html
          */
+        ArticleEntityExample example = new ArticleEntityExample();
+        example.setOrderByClause("update_time desc");
+        List<ArticleEntity>  list = blogService.selectByExample(example);
+        if (list != null && list.size() > 0) {
+            ArticleEntity articleEntity = list.get(0);
+            map.put("article",articleEntity);
+        }
         return "blog/index";
     }
+
     @RequestMapping("/archives")
     public String archives() {
         return "blog/archives";
--- src/main/java/com/bj/zzq/service/BlogService.java
+++ src/main/java/com/bj/zzq/service/BlogService.java
@@ -2,11 +2,19 @@ package com.bj.zzq.service;
 
 
 import com.bj.zzq.dao.ArticleEntityMapper;
+import com.bj.zzq.model.ArticleEntity;
+import com.bj.zzq.model.ArticleEntityExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BlogService {
     @Autowired
     private ArticleEntityMapper articleEntityMapper;
+
+    public List<ArticleEntity> selectByExample(ArticleEntityExample example) {
+        return articleEntityMapper.selectByExample(example);
+    }
 }
