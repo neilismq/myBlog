51d8ae5b582534090dfdd4ebdfa78b03fe7d54ec
zhaozhiqiang <zhaozhiqiang@kingxunlian.com>
2019-01-08 20:14:00 +0800

    1

--- src/main/java/com/bj/zzq/controller/AdminController.java
+++ src/main/java/com/bj/zzq/controller/AdminController.java
@@ -1,10 +1,17 @@
 package com.bj.zzq.controller;
 
+import com.bj.zzq.dao.ArticleDao;
 import com.bj.zzq.dao.UserDao;
 import com.bj.zzq.model.AdminEntity;
 import com.bj.zzq.model.AdminEntityExample;
+import com.bj.zzq.model.ArticleEntity;
+import com.bj.zzq.model.ArticleEntityExample;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.apache.commons.lang.StringUtils;
 import org.apache.tomcat.util.security.MD5Encoder;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +19,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping(value = "/admin")
 @Controller
 public class AdminController {
+
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private ArticleDao articleDao;
+
     /**
      * 登录
+     *
      * @param username
      * @param password
      * @return
@@ -51,6 +64,7 @@ public class AdminController {
 
     /**
      * 登陆页面
+     *
      * @return
      */
     @RequestMapping(value = "/login", method = RequestMethod.GET)
@@ -60,6 +74,7 @@ public class AdminController {
 
     /**
      * 后台首页
+     *
      * @return
      */
     @RequestMapping(value = "/index", method = RequestMethod.GET)
@@ -69,7 +84,28 @@ public class AdminController {
 
 
     @RequestMapping(value = "/article/add", method = RequestMethod.GET)
-    public String partial() {
+    public String articleAdd() {
         return "admin/articleManage/add";
+    }
+
+    @RequestMapping(value = "/article/query")
+    public String articleQuery(Map map, PageInfo pageInfo) {
+        if (pageInfo.getPageSize() == 0) {
+            pageInfo.setNavigatePages(5);
+            pageInfo.setPageSize(10);
+            pageInfo.setPageNum(1);
+        }
+        Page page = PageHelper.startPage(pageInfo.getPageNum(), pageInfo.getPageSize());
+        //分页合理化
+        page.setReasonable(true);
+
+        ArticleEntityExample articleEntityExample = new ArticleEntityExample();
+        articleEntityExample.setOrderByClause("create_time desc");
+        articleDao.selectArticleByExample(articleEntityExample);
+
+        PageInfo info = new PageInfo<>(page, pageInfo.getNavigatePages());
+        BeanUtils.copyProperties(info, pageInfo);
+        map.put("pageInfo", pageInfo);
+        return "admin/articleManage/query";
     }
 }
