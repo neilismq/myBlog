716f832e2a2b6d57883eed88928db8cb9aa66293
zhao <798828932@qq.com>
2019-06-18 13:01:00 +0800

    1

--- src/main/resources/design/myblog.sql
+++ src/main/resources/design/myblog.sql
@@ -1,21 +1,38 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : 本机mysql
  Source Server Type    : MySQL
- Source Server Version : 80011
+ Source Server Version : 50726
  Source Host           : localhost:3306
  Source Schema         : myblog
 
  Target Server Type    : MySQL
- Target Server Version : 80011
+ Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 17/11/2018 20:53:30
+ Date: 18/06/2019 12:33:11
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for blog_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `blog_admin`;
+CREATE TABLE `blog_admin`  (
+  `id` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `create_time` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of blog_admin
+-- ----------------------------
+INSERT INTO `blog_admin` VALUES ('1', 'admin', 'b63f84094d4ed7bf16c4c5e0be29f705', '2019-06-07 10:50:43');
 
 -- ----------------------------
 -- Table structure for blog_article
@@ -25,7 +42,8 @@ CREATE TABLE `blog_article`  (
   `id` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '文章id',
   `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文章标题',
   `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '作者名称',
-  `content` blob NULL COMMENT '文章内容',
+  `is_draft` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '是否是草稿 0-不是，1-是',
+  `content` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '文章内容',
   `create_time` timestamp(6) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` timestamp(6) NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
@@ -51,10 +69,25 @@ CREATE TABLE `blog_comment`  (
   `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '评论内容',
   `user_id` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '评论用户id',
   `article_id` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文章id',
+  `is_read` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '0-未读 1-已读',
   `create_time` timestamp(6) NULL DEFAULT NULL COMMENT '评论创建时间',
   `pid` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '回复的哪条评论（id）',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for blog_file
+-- ----------------------------
+DROP TABLE IF EXISTS `blog_file`;
+CREATE TABLE `blog_file`  (
+  `id` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件名称',
+  `origin_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件原名称',
+  `path` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件相对路径',
+  `size` int(11) NULL DEFAULT NULL COMMENT '文件大小',
+  `create_time` timestamp(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for blog_setting
@@ -62,6 +95,8 @@ CREATE TABLE `blog_comment`  (
 DROP TABLE IF EXISTS `blog_setting`;
 CREATE TABLE `blog_setting`  (
   `id` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'id',
+  `key` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `value` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -72,6 +107,7 @@ DROP TABLE IF EXISTS `blog_tag`;
 CREATE TABLE `blog_tag`  (
   `id` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标签id',
   `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标签名称',
+  `article_count` int(11) NULL DEFAULT NULL COMMENT '文章数量',
   `create_time` timestamp(6) NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
